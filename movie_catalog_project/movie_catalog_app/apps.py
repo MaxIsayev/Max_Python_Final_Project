04626642d4345bcdfebb2a533b0cb3528061bee0
MaxIsayev <max.i@list.ru>
2024-03-17 22:27:08 +0200

    app creation and configure

---
+++ movie_catalog_project/movie_catalog_app/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class MovieCatalogAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'movie_catalog_app'
+
+    class Meta:
+        verbose_name = _('movie_catalog_app')
